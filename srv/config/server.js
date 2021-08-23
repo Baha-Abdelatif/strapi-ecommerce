b995d55ee3af2823fdbfdedabe0f90b497a65170
Baha-Abdelatif <baha.abdelatif@gmail.com>
2021-08-23 07:15:23 +0200

    Backend init with placeholders products

---
+++ srv/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6497e9f259e6095adb246c66276c2bba'),
+    },
+  },
+});
---
+++ srv/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7e91c750-0085-44cc-bc4b-f76ea3de85eb'
+};
